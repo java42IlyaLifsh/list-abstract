bd46e4392e621f7da740ca943c79e3ce62435ac4
ilyal <ilyallifsh@gmail.com>
2021-11-12 11:33:26 +0200

    initial commit HW_12

---
+++ src/telran/util/AbstractList.java
@@ -0,0 +1,20 @@
+package telran.util;
+
+//HW_12 IlyaL  
+
+public abstract class AbstractList<T> implements List<T> {
+
+	protected int size;
+	
+	@Override
+	public int size() {
+		 
+		return size;
+	}
+	protected boolean isValidIndex (int index) {
+		return index >=0 && index < size;
+		
+	}
+	
+	
+}
